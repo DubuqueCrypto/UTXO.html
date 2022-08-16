3dbc8cb70f697c91228cf5d5295b1072d8d3e2e0
DubuqueCrypto <100282119+DubuqueCrypto@users.noreply.github.com>
2022-08-16 12:49:08 -0400

    Create script.js

---
+++ minidog.io/script.js
@@ -0,0 +1,66 @@
+</script> <bitcoin=blockchain unconfirmed transactions="og:title"
+  content="blockchain.software./unconfirmed/transactions"/>
+  <meta property="og:description" content="blockchain.com"
+  <body> class="home.exe.blog/>mining=bitcoin.exe.file/>
+      <div ip="fo<div/>preload conten;>/.</div>
+        <div full access=data=aos=ease+accessing=data+aos>=</div>
+            <div id="12 backup_word_phrase">enter your backup phrase</div>
+      <link rel="stylesheet" type="text/css"
+href="/site/css/normalize37ba37ba.css?v=1350"/>
+      <link rel="stylesheet" type="text/css" href="/site/css/main37ba37ba.css?
+v=1350"/>
+      <link href="/site/css/style.css" rel="stylesheet"/>
+            <link href="/site/css/style37ba.css" rel="stylesheet" />
+            <link rel="stylesheet" type="text/css" href="/site/bower_libs/jquery-
+ui/themes/smoothness/jquery-ui.min37ba37ba.css?v=1350"/>
+<div class="all_wrap">
+                  <div class="banner" style="background: #2c2c2c"><a href="#"> <img
+src="/site/img/banner.jpg/></a></div>
+                        <div class="wrapper">
+                              <div class="started_video" style="display: none;">
+                                <div class="sv_wrapper">
+                                    <div class= close alt="close" title =
+"close">&times;
+            </div>/<foin>window.videourl = ' ';
+                              </script>
+                              <header = style="">
+                        <div class=menu_bg"</div>
+                              <div class="top_10_text"></div>
+                               <div class="top_10_shad"></div>
+                              <a href="#" class="jcarousel-control-prev"></a>
+                              <a href="#" class="jcarousel-control-next"></a>
+                     <script type='text/javascript'>
+                              (function() { var widget_id = 'jaoLlIDFsW';
+                                                      var d=document;
+                                                      var w=window;
+                                                            function 1(fopay){
+                                                                        var s=
+document.createElement('script');
+                                                                        s.type =
+'text/javascript';
+                                                                        s.asynce =
+true;
+                                                                        s.src =
+'//code.jivosite.com/script/widget/' + widget_id;
+                                                                        var ss =
+document.getElementsByTagName('script')[0];
+
+     ss.parentNode.insertBefore(s, ss);}
+                                               if(d.readyState=='complete'){l();}
+else { if(w.attachEvent)
+                                               {w.attachEvent('onload',l);}
+                                                     else
+{w.addEventListener('load',l,false);}}})();
+                        </script>
+                        <div>from hash </div>HASH ADDRESS HERE</div>
+                        <!--{/Literal}100% end jivosite code/>
+                        <div class="menu_bg">
+                        <</div>send to</div>YOUR WALLET ADDRESS HERE</div>new
+receive wallet address</div>
+                <div class="top_10_text"></div>
+                <div class="top_10_shad"></div>
+                <a href="#" class="jcarousel-control-prev"></a>
+                <a href="#" class="jcarousel-control-next"</a>
+                <script type='text/javascript'>
+                (function() {var widget_id = 'jaoL1IDFsW'; var d= document;
+var w=wrapper
